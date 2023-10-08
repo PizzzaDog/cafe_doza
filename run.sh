2ed064d42671fe1865e90f088e02654cdd434e55
Nikolai Bogdan <nbogdan@spribe.io>
2023-10-08 19:19:58 +0200

    port changed

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ git pull
 docker stop doza_back_con || true
 docker rm doza_back_con || true
 docker build -t doza_back .
-docker run -d -p 8091:8091 --name doza_back_con doza_back
+docker run -d -p 8088:8088 --name doza_back_con doza_back
