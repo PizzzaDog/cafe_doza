c1419780f58787d8e5c1eddfa3a42cb6043a9a07
Nikolai Bogdan <nbogdan@spribe.io>
2023-10-13 00:09:49 +0200

    business logic first wave

--- src/main/java/by/pizzzadog/repository/UserRepository.java
+++ src/main/java/by/pizzzadog/repository/UserRepository.java
@@ -3,9 +3,11 @@ package by.pizzzadog.repository;
 import by.pizzzadog.model.MyUser;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<MyUser, String> {
 
 
