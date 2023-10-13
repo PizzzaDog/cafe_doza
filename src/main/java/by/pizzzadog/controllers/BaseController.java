881cf8c5a723056dc115299a1f16d4f44ca2f207
Nikolai Bogdan <nbogdan@spribe.io>
2023-10-13 13:18:25 +0200

    added role endpoint

--- src/main/java/by/pizzzadog/controllers/BaseController.java
+++ src/main/java/by/pizzzadog/controllers/BaseController.java
@@ -1,10 +1,12 @@
 package by.pizzzadog.controllers;
 
 import by.pizzzadog.constant.Routes;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = {Routes.API_ROUTE})
+@CrossOrigin
 public class BaseController {
 }
