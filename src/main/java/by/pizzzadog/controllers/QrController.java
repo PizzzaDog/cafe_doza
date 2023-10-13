4d596a40d60b009a9c21780890342334b936b43a
Nikolai Bogdan <nbogdan@spribe.io>
2023-10-13 15:01:10 +0200

    refresh user session endpoint

--- src/main/java/by/pizzzadog/controllers/QrController.java
+++ src/main/java/by/pizzzadog/controllers/QrController.java
@@ -6,6 +6,8 @@ import by.pizzzadog.model.QrRequestDto;
 import by.pizzzadog.service.QRService;
 import by.pizzzadog.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Log4j2
 @RestController
 @RequiredArgsConstructor
 public class QrController extends BaseController {
@@ -28,6 +31,7 @@ public class QrController extends BaseController {
 
     @PostMapping("/qr/get_user/{qrCode}")
     public SessionUserDto getUserByCode(@RequestBody LogTokenRequest auth, @PathVariable Long qrCode) {
+        log.info("POST ADMIN QR " + auth.getEmail() + auth.getToken() + qrCode);
         return userService.getByCodeAndAuth(qrCode, auth);
     }
 }
