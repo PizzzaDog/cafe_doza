c6654760886b17953f21f417aeb5ce275803373e
Nikolai Bogdan <nbogdan@spribe.io>
2023-10-13 10:29:28 +0200

    added role endpoint

--- src/main/java/by/pizzzadog/config/SecurityConfig.java
+++ src/main/java/by/pizzzadog/config/SecurityConfig.java
@@ -5,13 +5,13 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = false, securedEnabled = false)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
-
-    }
-}
+//@Configuration
+//@EnableGlobalMethodSecurity(prePostEnabled = false, securedEnabled = false)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+//
+//    }
+//}
--- src/main/java/by/pizzzadog/config/WebConfig.java
+++ src/main/java/by/pizzzadog/config/WebConfig.java
@@ -5,14 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-    }
-}
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig extends WebMvcConfigurerAdapter {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+//    }
+//}
